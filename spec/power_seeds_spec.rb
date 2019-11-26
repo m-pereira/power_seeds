28df9ad11fd861241866b2287c58ee24ff9ad51e
Mauricio P <mauriciopdvg@gmail.com>
2019-11-26 16:01:12 -0300

    add cc_test_reporter configs and .rubocop.yml

--- spec/power_seeds_spec.rb
+++ spec/power_seeds_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe PowerSeeds do
-  it "has a version number" do
+  it 'has a version number' do
     expect(PowerSeeds::VERSION).not_to be nil
   end
 end
